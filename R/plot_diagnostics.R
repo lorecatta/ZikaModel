8fc0f66797e8a70c2c51d6387d57a5f4eccd5c00
Lorenzo Cattarino <lcattari@dide.local>
2019-04-17 13:16:08 +0100

    add roxigen documentation

--- R/plot_diagnostics.R
+++ R/plot_diagnostics.R
@@ -1,31 +1,42 @@
+#------------------------------------------------
+#' plot_diagnostics
+#'
+#' \code{plot_diagnostics} makes a faceted plot of the model diagnostics.
+#'
+#' @param df The dataframe with the data to plot.
+#' @param diagno_nms A character string of descriptive names of the diagnostics. This will be used for the facet y labels.
+#' @param no_pages The number of pages over which to arrange the diagnostics.
+#'
+#' @inheritParams save_plot
+#'
+#' @importFrom ggplot2 aes scale_colour_manual scale_x_continuous
+#'   scale_y_continuous xlab ylab geom_line ggplot theme_bw
+#'   theme element_text margin unit ggtitle as_labeller
+#'
+#' @export
+
+
 plot_diagnostics <- function(df, out_pth, out_fl_nm, diagno_nms, no_pages){
-  
+
   # browser()
-  
+
   brks <- seq(from = 0, to = time, by = 364*10)
-  
+
   xstrips_labs <- as_labeller(setNames(diagno_nms, levels(df$diagnostics)))
-  
+
   dir.create(out_pth, FALSE, TRUE)
-  
+
   plot_ttl <- gsub(".*_", "", out_fl_nm)
-  
+
   for (i in seq_len(no_pages)){
-    
-    png(file.path(out_pth, sprintf("%s_%s%s", out_fl_nm, i, ".png")),
-        width = 23,
-        height = 12,
-        units = "cm",
-        pointsize = 12,
-        res = 300)
-    
+
     ret <- ggplot(df, aes(x = time, y = .data$value)) +
       geom_line(size = 0.4, colour = "#63B8FF") +
-      ggforce::facet_wrap_paginate(~ .data$diagnostics, 
-                                   ncol = 3, 
-                                   nrow = 3, 
-                                   scales = "free_y", 
-                                   labeller = xstrips_labs, 
+      ggforce::facet_wrap_paginate(~ .data$diagnostics,
+                                   ncol = 3,
+                                   nrow = 3,
+                                   scales = "free_y",
+                                   labeller = xstrips_labs,
                                    page = i) +
       scale_x_continuous(name = "Years", breaks = brks, labels = brks / 364) +
       scale_y_continuous(name = "", labels = scales::comma_format(accuracy = .01)) +
@@ -34,13 +45,15 @@ plot_diagnostics <- function(df, out_pth, out_fl_nm, diagno_nms, no_pages){
             axis.text.y = element_text(size = 8),
             strip.text.x = element_text(size = 8),
             plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
-            plot.title = element_text(margin = margin(0,0,0.5,0,"cm"))) + 
+            plot.title = element_text(margin = margin(0,0,0.5,0,"cm"))) +
       ggtitle(paste0("SEIR Zika model - ", plot_ttl))
-    
-    print(ret)
-    
-    dev.off()
-    
+
+    save_plot(ret,
+              out_pth,
+              out_fl_nm = sprintf("%s_%s%s", out_fl_nm, i, ".png"),
+              wdt = 23,
+              hgt = 12)
+
   }
-  
+
 }
