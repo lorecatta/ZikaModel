9ba764f0c5605908d1bb5b4c1a0576d485b8689e
Lorenzo Cattarino <lcattari@dide.local>
2019-04-17 12:22:22 +0100

    utility functions

---
+++ R/utilities.R
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------
+#
+# Utility functions
+#
+#------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------
+#' save_plot
+#'
+#' \code{save_plot} save a png file of a plot
+#'
+#' @param ret The plot object
+#' @param out_pth The path where to save the plot
+#' @param out_fl_nm The output file name
+#' @param wdt The plot width
+#' @param hgt The plot height
+#'
+#' @export
+
+
+save_plot <- function(ret, out_pth, out_fl_nm, wdt, hgt){
+
+  dir.create(out_pth, FALSE, TRUE)
+  png(file.path(out_pth, paste0(out_fl_nm, ".png")),
+      width = wdt,
+      height = hgt,
+      units = "cm",
+      pointsize = 12,
+      res = 300)
+  print(ret)
+  dev.off()
+
+}
