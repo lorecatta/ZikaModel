af4cf601ac71f0d6c4c68279d77477a2b78b6c51
Lorenzo Cattarino <lcattari@dide.local>
2019-05-02 12:29:09 +0100

    change BG_FOI to zero

--- R/create_model_parameters_list.R
+++ R/create_model_parameters_list.R
@@ -113,7 +113,7 @@ model_param_list_create <- function(
   other_prop_immune = 0.5,
   propTransGlobal = 0.0005,
   propTransNN = 0.005,
-  BG_FOI = 1e-6,
+  BG_FOI = 0,
   AGE_REC = 2,
   PropDiseaseReported = 0.1
 
