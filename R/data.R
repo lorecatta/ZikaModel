abc6bab979730bc81a60d4f8038ab34a8df083b1
Lorenzo Cattarino <lcattari@dide.local>
2019-06-21 16:45:02 +0100

    add dataset documentation

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Patches' nearest neighbors
+#'
+#' A matrix containing the id of the 8 nearest neighboring patches to each patch.
+#'  The variables are as follows:
+#'
+#' @format A matrix with 21 rows and 8 columns:
+#'
+#' @references \url{https://science.sciencemag.org/node/681801.full}
+"nn_links"
