0e5795760372541f8c7d0eab45638e61b81624a9
Lorenzo Cattarino <lcattari@dide.local>
2019-10-02 11:40:09 +0100

    do not attach .png

--- R/save_plot.R
+++ R/save_plot.R
@@ -16,7 +16,7 @@
 save_plot <- function(plot_obj, out_pth, out_fl_nm, wdt, hgt){
 
   dir.create(out_pth, FALSE, TRUE)
-  png(file.path(out_pth, paste0(out_fl_nm, ".png")),
+  png(file.path(out_pth, out_fl_nm),
       width = wdt,
       height = hgt,
       units = "cm",
