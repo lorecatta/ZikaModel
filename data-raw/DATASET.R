d3530c94992218844262736905a2c90cb621e947
Lorenzo Cattarino <lcattari@dide.local>
2019-11-11 15:00:58 +0000

    save amplitude and phases table

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,4 +2,7 @@
 
 nn_links <- read.table(file.path("data-raw", "nn_links.txt"), check.names = FALSE, header = TRUE)
 
+amplitudes_phases <- read.csv(file.path("data-raw", "amplitudes_phases.csv"))
+
 usethis::use_data(nn_links)
+usethis::use_data(amplitudes_phases)
