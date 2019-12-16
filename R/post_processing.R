398d763f1beb7ab2412a4e0619bdb1346c02aae2
Lorenzo Cattarino <lcattari@dide.local>
2019-12-16 13:48:39 +0000

    add function to postprocess mosquito outputs

--- R/post_processing.R
+++ R/post_processing.R
@@ -3,7 +3,7 @@
 
 #' The function processes model outputs to calculate metrics of interest.
 #'
-#' @title Post-processe model outputs
+#' @title Post-process model outputs
 #'
 #' @param dat list of model outputs from the model run.
 #'
@@ -70,5 +70,55 @@ post_processing <- function(dat, DT) {
   diagno_2 <- subset(df_H_melt, df_H_melt$diagnostic %in% c("Susceptibles", "Infectious", "Recovered", "Births", "Incidence of infections", "Total population", "Cumulative incidence", "Weekly infections/1000"))
 
   list("compartments" = droplevels(diagno_1), "demographics" = droplevels(diagno_2))
+
+}
+
+
+
+# -----------------------------------------------------------------------------
+
+#' The function processes model outputs to calculate metrics of interest for the
+#'  vector population.
+#'
+#' @title Post-process model outputs for mosquitoes
+#'
+#' @param dat list of model outputs from the model run.
+#'
+#' @importFrom stats setNames
+#'
+#' @export
+
+
+post_processing_mos <- function(dat) {
+
+  diagno_mos_wt <- c("Lwt", "Mwt_S", "Mwt_E1", "Mwt_E2", "Mwt_I1", "Mwt_tot", "Lwt_birth",
+                     "Lwt_mature", "Mwt_inf1")
+
+  diagno_mos_wb <- c("Lwb", "Mwb_S", "Mwb_E1", "Mwb_E2", "Mwb_I1", "Mwb_tot", "Lwb_birth",
+                     "Lwb_mature", "Mwb_inf1", "Mwb_intro")
+
+  diagno_mos <- c(diagno_mos_wt, diagno_mos_wb)
+
+  dia_mos <- setNames(dat[diagno_mos], diagno_mos)
+
+  mossum <- lapply(dia_mos, function(x){apply(x, 1, sum)})
+
+  mat_M <- do.call("cbind", mossum)
+
+  df_M <- as.data.frame(mat_M)
+
+  tt <- dat$TIME
+  time <- max(tt)
+
+  df_M$time <- tt
+  df_M_melt <- melt(df_M,
+                    id.vars = "time",
+                    variable.name = "diagnostic")
+
+  diagno_levs <- c(diagno_mos_wt, diagno_mos_wb)
+
+  df_M_melt$diagnostic <- factor(df_M_melt$diagnostic, levels = diagno_levs, labels = diagno_levs)
+
+  df_M_melt
 
 }
