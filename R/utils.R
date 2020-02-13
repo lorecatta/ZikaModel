f6a602e68ea8e76a7efaef30bdf8c5281f6a5c36
Lorenzo Cattarino <lcattari@dide.local>
2020-02-13 12:04:09 +0000

    add matrix support

--- R/utils.R
+++ R/utils.R
@@ -6,7 +6,8 @@
 #'
 #' @title Calculate lagged differences
 #'
-#' @param x vector of numeric values
+#' @param x vector or matrix of numeric values. In the case of a matrix,
+#'   the rows indicate the time-varying obeservations.
 #'
 #' @param lag the lag value (numeric)
 #'
@@ -17,8 +18,30 @@ lag_diff <- function(x, lag) {
 
   ret1 <- diff(x, lag = lag, differences = 1)
 
-  pad <- rep(0, lag)
+  if(is.matrix(x) | is.vector(x)) {
 
-  c(pad, ret1)
+    if(is.matrix(x)) {
+
+      pad <- matrix(0, nrow = lag, ncol = ncol(x))
+
+      out <- rbind(pad, ret1)
+
+    }
+
+    if(is.vectro(x)) {
+
+      pad <- rep(0, lag)
+
+      out <- c(pad, ret1)
+
+    }
+
+  } else {
+
+    stop("x is not a vector or a matrix")
+
+  }
+
+  out
 
 }
