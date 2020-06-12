b6fb22de6052399a2ea16485d8bfaa59f78ba91f
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-12 21:03:25 +0100

    add test

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,41 +1,18 @@
 test_that("run works", {
-
   set.seed(123)
-
   r1 <- run_deterministic_model()
 
+  expect_type(r1$output, "double")
 })
 
 test_that("dt is used correctly to calculate time steps", {
+  set.seed(123)
+  r2 <- run_deterministic_model()
 
-  age_init <- c(1, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+  mod <- r2$model
+  results <- mod$transform_variables(r2$output)
 
-  deathrt <- c(1e-10,
-               1e-10,
-               1e-10,
-               0.00277068683332695,
-               0.0210680857689784,
-               0.026724997685722,
-               0.0525354529367476,
-               0.0668013582441452,
-               0.119271483740379,
-               0.279105747097929,
-               0.390197266957464)
-
-  time_frame <- 364 * 10
-
-  tt <- seq(0, time_frame, 1)
-
-  mpl <- create_model_param_list(season = FALSE)
-
-  dt <- mpl$DT
-
-  model_outputs <- run_model(agec = age_init,
-                             death = deathrt,
-                             nn_links,
-                             amplitudes_phases,
-                             time = time_frame)
-
-  expect_identical(tt * dt, model_outputs$TIME)
-
+  max_t <- r2$parameters$time_period / r2$parameters$DT
+  tt <- seq(from = 1, to = max_t)
+  expect_identical(tt * r2$parameters$DT, results$TIME)
 })
