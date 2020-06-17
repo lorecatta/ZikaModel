9532a759c8afc850efe2e9e4ed429cf493591c54
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-17 14:20:21 +0100

    one test passed

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -16,3 +16,21 @@ test_that("dt is used correctly to calculate time steps", {
   tt <- seq(from = 1, to = max_t)
   expect_identical(tt * r2$parameters$DT, results$TIME)
 })
+
+test_that("format output works", {
+
+  set.seed(123)
+  r1 <- run_deterministic_model()
+
+  ## summarise by compartment
+  o1 <- format_output_H(r1)
+
+  n_comp <- unique(o1$compartment)
+  expect_type(o1, "data.frame")
+  expect_equal(now(o1), n_comp * r2$parameters$time_period)
+
+  ##summarise by patch
+  browser()
+  o2 <- format_output_H(r1, keep = "patch")
+
+})
