ff7946fae52a056608e582d845838d5369604fe0
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-18 16:31:14 +0100

    delete old format output functions

--- R/format_output.R
+++ R/format_output.R
@@ -274,115 +274,113 @@ format_output_M <- function(x,
 
 # -----------------------------------------------------------------------------
 
-#' The function processes model outputs to calculate metrics of interest.
+#' The function calculates the mean across patches of a model output,
+#' by ignoring the \emph{rest of the world} patch.
 #'
-#' @title Post-process model outputs
+#' @title Calculate the mean across patches of a model output
 #'
-#' @param dat list of model outputs from the model run.
-#'
-#' @inheritParams parameters_deterministic_model
+#' @param my_vector vector of model outputs by patch.
 #'
 #' @export
-post_processing <- function(dat, DT) {
+calculate_mean_of_patch_variables <- function(my_vector) {
 
-  weekly_lag_time <- 7 / DT
+  NP <- length(my_vector)
 
-  tt <- dat$TIME
-  time <- max(tt)
+  ret <- (sum(my_vector) - my_vector[NP]) / (NP - 1)
 
-  diagno_hum <- c("S", "I1", "R1", "births", "inf_1")
-
-  dia_hum <- setNames(dat[diagno_hum], diagno_hum)
-
-  # cum sum over the first dimension - specify the dims you want to keep
-  # no need for aperm reshaping here
-  inf_1_cum <- apply(dia_hum$inf_1, c(2, 3, 4), cumsum)
-
-  Nt <- dia_hum$S + dia_hum$I1 + dia_hum$R1
-
-  dia_hum <- c(dia_hum,
-               list(Nt = Nt),
-               list(inf_1_cum = inf_1_cum))
-
-  humsum <- lapply(dia_hum, function(x){apply(x, 1, sum)})
-
-  mat_H <- do.call("cbind", humsum)
-
-  prop <- mat_H[, c("S", "I1", "R1")] / mat_H[, "Nt"]
-
-  colnames(prop) <- c("Sp", "I1p", "R1p")
-
-  df_H <- as.data.frame(cbind(mat_H, prop))
-
-  # rate of total weekly infections
-  df_H$wIR_inf <- lag_diff(df_H$inf_1_cum, weekly_lag_time)
-
-  df_H$wIR_inf <- df_H$wIR_inf / df_H$Nt * 1000
-
-  df_H$time <- tt
-  df_H_melt <- reshape2::melt(df_H,
-                              id.vars = "time",
-                              variable.name = "diagnostic")
-
-  current_levs <- c("S", "I1", "R1", "births", "inf_1", "Nt", "inf_1_cum", "Sp", "I1p", "R1p", "wIR_inf")
-
-  # rename
-  new_levs <- c("Susceptibles", "Infectious", "Recovered", "Births", "Incidence of infections", "Total population", "Cumulative incidence", "Sp", "I1p", "R1p", "Weekly infections/1000")
-
-  levels(df_H_melt$diagnostic) <- new_levs
-
-  # reorder
-  df_H_melt$diagnostic <- factor(df_H_melt$diagnostic, levels = c("Susceptibles", "Infectious", "Recovered", "Total population", "Births", "Incidence of infections", "Cumulative incidence", "Weekly infections/1000", "Sp", "I1p", "R1p"))
-
-  diagno_1 <- subset(df_H_melt, df_H_melt$diagnostic %in% c("Sp", "I1p", "R1p"))
-
-  diagno_2 <- subset(df_H_melt, df_H_melt$diagnostic %in% c("Susceptibles", "Infectious", "Recovered", "Births", "Incidence of infections", "Total population", "Cumulative incidence", "Weekly infections/1000"))
-
-  list("compartments" = droplevels(diagno_1), "demographics" = droplevels(diagno_2))
+  return(ret)
 
 }
 
 
 # -----------------------------------------------------------------------------
 
-#' The function processes model outputs to calculate metrics of interest for the
-#'  vector population.
+#' The function applies a sum over the margin of an array.
 #'
-#' @title Post-process model outputs for mosquitoes
+#' @title Sum over aray margin
 #'
-#' @param dat list of model outputs from the model run.
+#' @param my_array array to sum.
+#'
+#' @param keep character vector of which variable / dimension to keep.
+#'
+#' @param compartment character vector of compartment name.
 #'
 #' @export
-post_processing_mos <- function(dat) {
+sum_across_array_dims <- function(my_array, keep = NULL, compartment = NULL) {
 
-  diagno_mos_wt <- c("Lwt", "Mwt_S", "Mwt_E1", "Mwt_E2", "Mwt_I1", "Mwt_tot", "Lwt_birth",
-                     "Lwt_mature", "Mwt_inf1")
+  summary_vars_to_average <- c("Kc", "eip", "Delta", "R0t_1", "FOI1")
 
-  diagno_mos_wb <- c("Lwb", "Mwb_S", "Mwb_E1", "Mwb_E2", "Mwb_I1", "Mwb_tot", "Lwb_birth",
-                     "Lwb_mature", "Mwb_inf1", "Mwb_intro")
+  no_array_dims <- length(dim(my_array))
 
-  diagno_mos <- c(diagno_mos_wt, diagno_mos_wb)
+  if (!is.null(keep) && (no_array_dims == 2 &  keep == "vaccine"))
+    stop("Can not summarise mosquito-related variables or compartments by vaccine status")
 
-  dia_mos <- setNames(dat[diagno_mos], diagno_mos)
+  if (no_array_dims == 2) {
 
-  mossum <- lapply(dia_mos, function(x){apply(x, 1, sum)})
+    if(is.null(keep)) {
 
-  mat_M <- do.call("cbind", mossum)
+      if(compartment %in% summary_vars_to_average) {
 
-  df_M <- as.data.frame(mat_M)
+        ret <- apply(my_array, 1, calculate_mean_of_patch_variables)
 
-  tt <- dat$TIME
-  time <- max(tt)
+      } else {
 
-  df_M$time <- tt
-  df_M_melt <- melt(df_M,
-                    id.vars = "time",
-                    variable.name = "diagnostic")
+        ret <- apply(my_array, 1, sum)
 
-  diagno_levs <- c(diagno_mos_wt, diagno_mos_wb)
+      }
 
-  df_M_melt$diagnostic <- factor(df_M_melt$diagnostic, levels = diagno_levs, labels = diagno_levs)
+    } else if (keep == "patch") {
 
-  df_M_melt
+      ret <- my_array
+
+    }
+
+  } else {
+
+    if (is.null(keep)) {
+
+      ret <- apply(my_array, 1, sum)
+
+    } else if (keep == "patch") {
+
+      ret <- apply(my_array, c(1, 4), sum)
+
+    } else if (keep == "vaccine") {
+
+      ret <- apply(my_array, c(1, 3), sum)
+
+    }
+
+  }
+
+  ret
+
+}
+
+
+# -----------------------------------------------------------------------------
+
+#' The function applies a cumulative sum over the margin of an array.
+#'
+#' @title Cumulative sum over array margin
+#'
+#' @param my_array array to sum.
+#'
+#' @param keep character vector of which variable / dimension to keep
+#'
+#' @export
+cumsum_across_array_dims <- function(my_array, keep = NULL) {
+
+  if (is.null(keep)) {
+
+    ret <- cumsum(my_array)
+
+  } else if (keep == "patch" | keep == "vaccine") {
+
+    ret <- apply(my_array, 2, cumsum)
+
+  }
+
+  ret
 
 }
