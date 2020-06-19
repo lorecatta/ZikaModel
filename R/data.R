0a409448a7531483df60aafb94650d4f891f0c9f
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-19 13:19:01 +0100

    document additional datasets

--- R/data.R
+++ R/data.R
@@ -14,3 +14,18 @@
 #'
 #' @format A matrix with 21 rows and 3 columns:
 "amplitudes_phases"
+
+#' Brazil birth rates
+#'
+#' A dataset containing the per capita birth rates for 11 age classes for Brazil.
+#'
+#' @format A numeric vector of length 11:
+"br_brazil_age"
+
+#' ZIKV-induced microcephaly risk during pregancy
+#'
+#' A dataset containing the probability that a foetus developes microcephaly
+#' given that the mother is infected with ZIKV in a particular day of pregnancy.
+#'
+#' @format A dataframe of 280 rows and 2 columns:
+"mc_prob_ZIKV_pregn"
