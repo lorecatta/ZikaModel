85b8ecd8e2278b30f667c4c538c7de3455db5149
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2020-06-22 15:54:17 +0100

    move output tests to a different test file

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -10,88 +10,28 @@ test_that("run works", {
 test_that("dt is used correctly to calculate time steps", {
 
   set.seed(123)
-  r2 <- run_deterministic_model()
+  r1 <- run_deterministic_model()
 
-  mod <- r2$model
-  results <- mod$transform_variables(r2$output)
+  mod <- r1$model
+  results <- mod$transform_variables(r1$output)
 
-  max_t <- r2$parameters$time_period / r2$parameters$DT
+  max_t <- r1$parameters$time_period / r1$parameters$DT
   tt <- seq(from = 1, to = max_t)
-  expect_identical(tt * r2$parameters$DT, results$time)
+  expect_identical(tt * r1$parameters$DT, results$time)
 
 })
 
-test_that("format output works", {
+test_that("there are no infections when BG_FOI is 0", {
 
-  set.seed(123)
-  r1 <- run_deterministic_model(time_period = 100)
+  r1 <- run_deterministic_model(BG_FOI = 0)
 
-
-  ### only human compartments
-
-
-  ## summarise by compartment
   o1 <- format_output_H(r1)
+  expect_true(all(o1[o1$compartment == "I1", "y"] == 0))
 
-  expect_s3_class(o1, "data.frame")
-  expect_equal(sum(o1$compartment == "S"), 100)
-  expect_equal(sum(o1$compartment == "I1"), 100)
-  expect_equal(sum(o1$compartment == "R1"), 100)
-
-
-  ## summarise by patch
-  o2 <- format_output_H(r1, keep = "patch")
-  expect_equal(dim(o2), c(6300, 4))
-
-  ## summarise by vaccine status
-  o3 <- format_output_H(r1, keep = "vaccine")
-  expect_equal(dim(o3), c(600, 4))
-
-
-  ### selected variables
-
-
-  ## summarise by compartment
-  o4 <- format_output_H(r1, var_select = c("S", "R1", "inf_1", "MC_w"))
-  expect_equal(dim(o4), c(400, 3))
-
-  ## summarise by patch
-  o5 <- format_output_H(r1, var_select = c("S", "R1", "inf_1", "MC_w"), keep = "patch")
-  expect_equal(dim(o5), c(8400, 4))
-
-  ## summarise by vaccine status
-  o6 <- format_output_H(r1, var_select = c("S", "R1", "inf_1", "MC_w"), keep = "vaccine")
-  expect_equal(sum(o6$compartment == "S"), 200)
-  expect_equal(sum(o6$compartment == "R1"), 200)
-  expect_equal(sum(o6$compartment == "inf_1"), 200)
-  expect_equal(sum(o6$compartment == "MC_w"), 200)
-
-
-  #### mosquitoes
-
-
-  ## summarise by compartment
-  o7 <- format_output_M(r1)
-  expect_equal(dim(o7), c(800, 3))
-
-  ## summarise by patch
-  o8 <- format_output_M(r1, keep = "patch")
-  expect_equal(dim(o8), c(16800, 4))
-
-
-  ### selected variables
-
-
-  ## summarise by compartment
-  o9 <- format_output_M(r1, var_select = c("Lwt", "Mwt_E2", "Mwb_S", "Delta"))
-  expect_equal(sum(o9$compartment == "Lwt"), 100)
-  expect_equal(sum(o9$compartment == "Mwt_E2"), 100)
-  expect_equal(sum(o9$compartment == "Mwb_S"), 100)
-  expect_equal(sum(o9$compartment == "Delta"), 100)
-
-
-  ## summarise by patch
-  o10 <- format_output_M(r1, var_select = c("Lwt", "Mwt_E2", "Mwb_S", "Delta"), keep = "patch")
-  expect_equal(dim(o10), c(8400, 4))
+  mod <- r1$model
+  results <- mod$transform_variables(r1$output)
+  t_max <- 365
+  expect_equal(results$I1[t_max,,,], r1$parameters$init_I1)
+  expect_equal(results$S[t_max,,,], r1$parameters$init_S)
 
 })
